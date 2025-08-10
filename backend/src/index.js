1328c9aac55843480a50f14ffc3b9b7958dd16ed
@Emad54 <549043@student.saxion.nl>
2025-08-10 11:21:06 +0200

    add Dockerfile and configure containerized backend with SQLite volume mount

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,16 +11,16 @@ app.use(cors());
 app.use('/', routes);
 
 app.use((req, res) => {
-    res.status(statusCodes.NOT_FOUND).json({ error: 'Endpoint not found' });
+    res.status(statusCodes.NOT_FOUND).json({error: 'Endpoint not found'});
 });
 
 // Global error handler
 app.use((err, req, res, next) => {
     res
         .status(err.status || statusCodes.INTERNAL_SERVER_ERROR)
-        .json({ message: err.message || 'Something went wrong!' });
+        .json({message: err.message || 'Something went wrong!'});
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server listening on port ${port}!`);
 });
